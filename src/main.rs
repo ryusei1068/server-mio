c3099ee85142ece04fbbfcb4b9c2ac46b4ae51b0
Ryusei Nohara <nhrryi@gmail.com>
2024-11-10 14:11:36 +0900

    refcell

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use core::str;
 use mio::net::{TcpListener, TcpStream};
 use mio::{event::Event, Events, Interest, Poll, Token};
+use std::cell::RefCell;
 use std::collections::{HashMap, VecDeque};
 use std::io::{Read, Write};
 use std::os::fd::AsRawFd;
+use std::rc::Rc;
 
 const SERVER: Token = Token(0);
 
+#[derive(Debug)]
 struct User {
     stream: TcpStream,
     name: Option<String>,
@@ -42,34 +45,87 @@ impl User {
         }
         Ok(())
     }
+
+    fn read(&mut self) -> std::io::Result<Option<String>> {
+        let mut buf = [0; 256];
+        let data = match self.stream.read(&mut buf) {
+            Ok(n) => std::str::from_utf8(&buf[0..n]).expect("ignoring error"),
+            Err(ref e) if would_block(e) => "",
+            Err(e) => {
+                eprintln!("Error reading from socket: {}", e);
+                return Err(e);
+            }
+        };
+
+        if self.name.is_none() {
+            self.name = Some(data.to_string());
+            println!("User connected with name: {:?}", self.name);
+            Ok(None)
+        } else {
+            println!("Received data: {:?}", data);
+            Ok(Some(data.to_string()))
+        }
+    }
 }
 
 struct Server {
-    sockets: HashMap<Token, User>,
+    sockets: Rc<RefCell<HashMap<Token, Rc<RefCell<User>>>>>,
     poll: Poll,
 }
 
 impl Server {
     fn new() -> std::io::Result<Self> {
         Ok(Server {
-            sockets: HashMap::new(),
+            sockets: Rc::new(RefCell::new(HashMap::new())),
             poll: Poll::new()?,
         })
     }
 
+    fn broadcast(&mut self, msg: String, cur_token: Token) -> std::io::Result<()> {
+        for (token, other_user) in self.sockets.clone().borrow().iter() {
+            if *token != cur_token {
+                other_user.borrow_mut().push_message(msg.clone());
+                self.reregister_rw(other_user.clone(), *token)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn reregister_rw(&mut self, user: Rc<RefCell<User>>, token: Token) -> std::io::Result<()> {
+        println!("{:?}", user.clone().borrow());
+        self.poll.registry().reregister(
+            &mut user.borrow_mut().stream,
+            token,
+            Interest::READABLE | Interest::WRITABLE,
+        )?;
+
+        Ok(())
+    }
+
+    fn register_rw(&mut self, user: Rc<RefCell<User>>, token: Token) -> std::io::Result<()> {
+        println!("{:?}", user.clone().borrow());
+        self.poll.registry().register(
+            &mut user.borrow_mut().stream,
+            token,
+            Interest::READABLE | Interest::WRITABLE,
+        )?;
+
+        Ok(())
+    }
+
     fn handle_new_connections(&mut self, listener: &TcpListener) -> std::io::Result<()> {
         match listener.accept() {
-            Ok((mut stream, _)) => {
+            Ok((stream, _)) => {
                 let fd = stream.as_raw_fd();
-                let client = Token(fd as usize);
-                self.poll.registry().register(
-                    &mut stream,
-                    client,
-                    Interest::READABLE | Interest::WRITABLE,
-                )?;
-                let mut user = User::new(stream);
-                user.push_message("Please enter your name: ".into());
-                self.sockets.insert(client, user);
+                let token = Token(fd as usize);
+
+                let user = Rc::new(RefCell::new(User::new(stream)));
+                self.register_rw(user.clone(), token)?;
+                user.borrow_mut()
+                    .push_message("Please enter your name: ".into());
+                self.sockets.clone().borrow_mut().insert(token, user);
+
                 println!("Got a connection from: {}", fd);
             }
             Err(ref err) if would_block(err) => {}
@@ -82,60 +138,36 @@ impl Server {
 
     fn handle_user_event(&mut self, client: Token, event: &Event) -> std::io::Result<()> {
         if event.is_read_closed() {
-            self.sockets.remove(&client);
+            self.sockets.clone().borrow_mut().remove(&client);
             println!("Connection closed fd: {:?}", client.0);
             return Ok(());
         }
 
         if event.is_readable() {
-            if let Some(user) = self.sockets.get_mut(&client) {
-                let mut buf = [0; 256];
-                match user.stream.read(&mut buf) {
-                    Ok(n) => {
-                        if user.name.is_none() {
-                            if let Ok(name) = str::from_utf8(&buf[..n]) {
-                                user.name = Some(name.trim().to_string());
-                                println!("User connected with name: {}", name.trim());
-                                user.push_message(
-                                    "Welcome, you can start sending messages.\n".into(),
-                                );
-                                self.poll.registry().reregister(
-                                    &mut user.stream,
-                                    client,
-                                    Interest::READABLE | Interest::WRITABLE,
-                                )?;
-                            }
-                        } else {
-                            println!("Received data: {:?}", &buf[0..n]);
-                            let message = format!(
-                                "{}: {}",
-                                user.name.clone().unwrap_or_default(),
-                                String::from_utf8_lossy(&buf[..n])
-                            );
-                            for (token, other_user) in self.sockets.iter_mut() {
-                                if *token != client {
-                                    other_user.push_message(message.clone());
-                                    self.poll.registry().reregister(
-                                        &mut other_user.stream,
-                                        *token,
-                                        Interest::READABLE | Interest::WRITABLE,
-                                    )?;
-                                }
-                            }
-                        }
-                    }
-                    Err(ref e) if would_block(e) => {}
-                    Err(e) => {
-                        eprintln!("Error reading from socket: {}", e);
-                        self.sockets.remove(&client);
-                    }
+            if let Some(user) = self.sockets.clone().borrow().get(&client) {
+                let msg = user.borrow_mut().read()?;
+
+                if msg.is_none() {
+                    user.borrow_mut()
+                        .push_message("Welcome, you can start sending messages.\n".into());
+                    self.reregister_rw(user.clone(), client)?;
+                }
+
+                if let Some(message) = msg {
+                    println!("Received data: {:?}", message);
+                    let message = format!(
+                        "{}: {}",
+                        user.borrow().name.clone().unwrap_or_default(),
+                        message
+                    );
+                    self.broadcast(message, client)?;
                 }
             }
         }
 
         if event.is_writable() {
-            if let Some(user) = self.sockets.get_mut(&client) {
-                user.write()?;
+            if let Some(user) = self.sockets.clone().borrow_mut().get_mut(&client) {
+                user.borrow_mut().write()?;
             }
         }
         Ok(())
